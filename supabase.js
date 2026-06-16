2f2606ea3ffc87792624d4a77e47ee3f416d40d4
pimjaisdd2542-max <pimjai.sdd2542@gmail.com>
2026-06-16 10:49:36 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://tzpzdstmtktngzdlhrgi.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6cHpkc3RtdGt0bmd6ZGxocmdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0ODczNTcsImV4cCI6MjA5NzA2MzM1N30.pQvec8YNWgiuzztEyrMsUGqiH3oMP8z8QitpPMdpyYs";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
