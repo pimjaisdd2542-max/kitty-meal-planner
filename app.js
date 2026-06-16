d2957acc2e0ba0e617176cfe291b06a4ca50eefc
pimjaisdd2542-max <pimjai.sdd2542@gmail.com>
2026-06-16 10:50:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Kitty Meal Planner Loaded 🐱");
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const foodsDiv = document.getElementById("foods");
+
+    const { data, error } = await supabaseClient
+        .from("foods")
+        .select("*");
+
+    if (error) {
+        foodsDiv.innerHTML = `
+            <p>❌ ${error.message}</p>
+        `;
+        return;
+    }
+
+    let html = "";
+
+    data.forEach(food => {
+        html += `
+            <div style="
+                background:white;
+                padding:15px;
+                margin:10px 0;
+                border-radius:15px;
+            ">
+                <h3>${food.name}</h3>
+                <p>🔥 ${food.calories} kcal</p>
+                <p>💪 ${food.protein} g</p>
+            </div>
+        `;
+    });
+
+    foodsDiv.innerHTML = html;
+
 });
