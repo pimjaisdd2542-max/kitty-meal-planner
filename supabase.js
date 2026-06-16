4c0230ed369b97f6a172c9bdf2835f7b30bb951b
pimjaisdd2542-max <pimjai.sdd2542@gmail.com>
2026-06-16 10:48:12 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,35 @@
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+document.addEventListener("DOMContentLoaded", async () => {
 
-console.log("Supabase Ready");
+    const foodsDiv = document.getElementById("foods");
+
+    const { data, error } = await supabaseClient
+        .from("foods")
+        .select("*");
+
+    if (error) {
+        foodsDiv.innerHTML = `
+            <p>❌ ${error.message}</p>
+        `;
+        return;
+    }
+
+    let html = "";
+
+    data.forEach(food => {
+        html += `
+            <div style="
+                background:white;
+                padding:15px;
+                margin:10px 0;
+                border-radius:15px;
+            ">
+                <h3>${food.name}</h3>
+                <p>🔥 ${food.calories} kcal</p>
+                <p>💪 ${food.protein} g</p>
+            </div>
+        `;
+    });
+
+    foodsDiv.innerHTML = html;
+
+});
