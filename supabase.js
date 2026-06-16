fb007285d0c334084fe3c1b8d9bd4557e44e5add
pimjaisdd2542-max <pimjai.sdd2542@gmail.com>
2026-06-16 10:49:13 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,35 +1,7 @@
-document.addEventListener("DOMContentLoaded", async () => {
+const SUPABASE_URL = "https://tzpzdstmtktngzdlhrgi.supabase.co";
+const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
 
-    const foodsDiv = document.getElementById("foods");
-
-    const { data, error } = await supabaseClient
-        .from("foods")
-        .select("*");
-
-    if (error) {
-        foodsDiv.innerHTML = `
-            <p>❌ ${error.message}</p>
-        `;
-        return;
-    }
-
-    let html = "";
-
-    data.forEach(food => {
-        html += `
-            <div style="
-                background:white;
-                padding:15px;
-                margin:10px 0;
-                border-radius:15px;
-            ">
-                <h3>${food.name}</h3>
-                <p>🔥 ${food.calories} kcal</p>
-                <p>💪 ${food.protein} g</p>
-            </div>
-        `;
-    });
-
-    foodsDiv.innerHTML = html;
-
-});
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
