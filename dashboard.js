841f451bad7bc6948ca83a9406589c4007b530d3
pimjaisdd2542-max <pimjai.sdd2542@gmail.com>
2026-06-16 13:25:18 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", loadDashboard);
 
+async function loadDashboard(){
+
+    const { data } = await supabaseClient
+        .from("foods")
+        .select("*");
+
+    const foods = data || [];
+
+    document.getElementById("foodCount").textContent =
+        foods.length;
+
+    document.getElementById("favoriteCount").textContent =
+        foods.filter(x => x.favorite).length;
+}
